f86302b98e7289760e25bf61262b22782dbc3f9a
ckdtjs505 <ckdtjs505@naver.com>
2019-11-26 21:50:29 +0900

    [CREATE] express routes 구현 및 작동

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ function handleListening (){
     console.log('Listening on')
 }
 
+function handleHome (req, res){
+    res.send('hello from home');
+}
+
+function handleProfile ( req,res ){
+    res.send("you are on my profile");
+}
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT,handleListening );
