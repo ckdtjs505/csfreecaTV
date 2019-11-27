07ab2911b7c6e9be78be57c2d798c11e81721b84
ckdtjs505 <ckdtjs505@naver.com>
2019-11-27 23:10:20 +0900

    [CREATE] 미들웨어어 추가

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import morgan  from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+
 const app = express();
 
 const PORT = 4000;
@@ -7,6 +13,12 @@ const handleListening = () => console.log('Listening on');
 const handleHome = (req, res) => res.send('hello from home');
 const handleProfile = ( req,res ) => res.send("you are on my profile");
 
-app.get("/", handleHome);
+app.use(cookieParser());
+app.use(bodyParser.json({extended : true}))
+app.use(bodyParser.urlencoded( { extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get("/",handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT,handleListening );
