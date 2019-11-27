fc14c4490fbf593f8860f80c6a21e53f6a4ac1f6
ckdtjs505 <ckdtjs505@naver.com>
2019-11-27 22:52:01 +0900

    [UPDATE] ES5 -> ES6 변환

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const app = express();
 
 const PORT = 4000;
 
-function handleListening (){
-    console.log('Listening on')
-}
+const handleListening = () => console.log('Listening on');
+const handleHome = (req, res) => res.send('hello from home');
+const handleProfile = ( req,res ) => res.send("you are on my profile");
 
-function handleHome (req, res){
-    res.send('hello from home');
-}
-
-function handleProfile ( req,res ){
-    res.send("you are on my profile");
-}
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
-
 app.listen(PORT,handleListening );
