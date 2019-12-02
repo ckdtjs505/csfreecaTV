ed33bdb73377ba8c69182ac096f1fb009b9f33d5
ckdtjs505 <ckdtjs505@naver.com>
2019-12-02 23:04:38 +0900

    [CREATE] all page create, layout templete 완성

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,7 @@
-export const join = (req, res) => res.send('join');
-export const login = (req, res) => res.send('login');
-export const logout = (req, res) => res.send('logout');
-
-export const users = (req, res) => res.send('users');
-export const userDetail = (req, res) => res.send('userDetail');
-export const editProfile = (req, res) => res.send('editProfile');
-export const changePassword = (req, res) => res.send('changePassword');
+export const join = (req, res) => res.render('join');
+export const login = (req, res) => res.render('login');
+export const logout = (req, res) => res.render('logout');
+export const users = (req, res) => res.render('users');
+export const userDetail = (req, res) => res.render('userDetail');
+export const editProfile = (req, res) => res.render('editProfile');
+export const changePassword = (req, res) => res.render('changePassword');
