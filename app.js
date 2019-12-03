5f8b4af53c0a78b6553fca366adf6cc78e6e5bcf
ckdtjs505 <ckdtjs505@naver.com>
2019-12-03 23:49:08 +0900

    [CREATE] local 변수 선언, 페이지 네이션 구현

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ import morgan  from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localMiddleware } from './localMiddleware';
+import routes from './routes';
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
-import routes from './routes';
 const app = express();
 
+app.use(helmet());
 app.set('view engine', "pug");
 app.use(cookieParser());
 app.use(bodyParser.json({extended : true}))
 app.use(bodyParser.urlencoded( { extended: true }));
-app.use(helmet());
 app.use(morgan('dev'));
+
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.videos, videoRouter);
---
+++ localMiddleware.js
@@ -0,0 +1,7 @@
+import routes from "./routes";
+
+export const localMiddleware = (req, res, next) => {
+    res.locals.title = "csTube";
+    res.locals.routes = routes;
+    next();
+};
