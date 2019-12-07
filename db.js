6476bde5ed8304cee4b94e02f9b6b8a991b93565
ckdtjs505 <ckdtjs505@naver.com>
2019-12-08 00:47:18 +0900

    [UPDATE] video block 완성

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ export const videos = [
   {
     id: 1323123,
     title: "video 1",
-    decription: "this is someting i love",
+    description: "this is someting i love",
     views: 24,
     videofile:
       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
@@ -15,7 +15,7 @@ export const videos = [
   {
     id: 1323,
     title: "video 2",
-    decription: "this is someting i love",
+    description: "this is someting i love",
     views: 24,
     videofile:
       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
@@ -28,7 +28,7 @@ export const videos = [
   {
     id: 132423,
     title: "video 3",
-    decription: "this is someting i love",
+    description: "this is someting i love",
     views: 24,
     videofile:
       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
@@ -41,7 +41,7 @@ export const videos = [
   {
     id: 312239,
     title: "video 4",
-    decription: "this is someting i love",
+    description: "this is someting i love",
     views: 24,
     videofile:
       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
@@ -54,7 +54,7 @@ export const videos = [
   {
     id: 6451,
     title: "video 5",
-    decription: "this is someting i love",
+    description: "this is someting i love",
     views: 24,
     videofile:
       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
