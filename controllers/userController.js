bcc75c67821f45cea105bb120fb337ce3c36ff39
ckdtjs505 <ckdtjs505@naver.com>
2019-12-08 10:06:10 +0900

    [UPDATE] join page controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,31 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "UserDetail" });
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "EditProfile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "ChangePassword" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import {
+  login,
+  logout,
+  getJoin,
+  postJoin
+} from "../controllers/userController";
 
 const grobalRouter = express.Router();
 
 grobalRouter.get(routes.home, home);
-grobalRouter.get(routes.join, join);
+grobalRouter.get(routes.join, getJoin);
+grobalRouter.post(routes.join, postJoin);
 grobalRouter.get(routes.login, login);
 grobalRouter.get(routes.logout, logout);
 grobalRouter.get(routes.search, search);
