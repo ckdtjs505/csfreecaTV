49547e2ad7b46886bd429b54331b21614d5cb1d5
ckdtjs505 <ckdtjs505@naver.com>
2019-12-08 09:48:40 +0900

    [UPDATE] search page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,18 +1,23 @@
 import { videos } from "../db";
+
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
+
 export const search = (req, res) => {
   const {
     query: { search_query: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Deletet Video" });
