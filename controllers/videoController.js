726150f84f7e888317eb28196ffd2d02593f4f6f
ckdtjs505 <ckdtjs505@naver.com>
2019-12-10 16:25:54 +0900

    [UPDATE] upload controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
@@ -10,8 +11,15 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  res.redirect(routes.videoDetail(123));
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
