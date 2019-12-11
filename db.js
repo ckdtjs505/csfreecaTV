3aec85a07982321a5e2eb298017207cd7af43c20
ckdtjs505 <ckdtjs505@naver.com>
2019-12-11 23:38:29 +0900

    [CREATE] mongodb, mongoose and connect

--- db.js
+++ db.js
@@ -1,67 +1,15 @@
-export const videos = [
-  {
-    id: 1323123,
-    title: "video 1",
-    description: "this is someting i love",
-    views: 24,
-    videofile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123213,
-      name: "changsun",
-      email: "ckdtjs505@naver.com"
-    }
-  },
-  {
-    id: 1323,
-    title: "video 2",
-    description: "this is someting i love",
-    views: 24,
-    videofile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123213,
-      name: "changsun",
-      email: "ckdtjs505@naver.com"
-    }
-  },
-  {
-    id: 132423,
-    title: "video 3",
-    description: "this is someting i love",
-    views: 24,
-    videofile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123213,
-      name: "changsun",
-      email: "ckdtjs505@naver.com"
-    }
-  },
-  {
-    id: 312239,
-    title: "video 4",
-    description: "this is someting i love",
-    views: 24,
-    videofile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123213,
-      name: "changsun",
-      email: "ckdtjs505@naver.com"
-    }
-  },
-  {
-    id: 6451,
-    title: "video 5",
-    description: "this is someting i love",
-    views: 24,
-    videofile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creater: {
-      id: 123213,
-      name: "changsun",
-      email: "ckdtjs505@naver.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/cstube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ Connect to DB");
+const handleError = error => console.log(`😂 Error on DB connection :${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
