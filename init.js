fd2333ccb523178bc370f5a17f1a65b4950b313d
ckdtjs505 <ckdtjs505@naver.com>
2019-12-19 23:14:10 +0900

    feat: add webpack module

--- init.js
+++ init.js
@@ -1,12 +1,12 @@
 import "./db";
-import app from "./app";
 import dotenv from "dotenv";
+import app from "./app";
 import "./models/Video";
 import "./models/Comment";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 
 const handleListening = () =>
   console.log(`✅  Listening on: http://localhost:${PORT} `);
