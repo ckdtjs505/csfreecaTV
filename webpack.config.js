525d3b62a6998b12a72d2af206605ff8b1e8f0f5
ckdtjs505 <ckdtjs505@naver.com>
2019-12-20 15:55:55 +0900

    Fix: webpack autoprefixer-browsers error

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const config = {
             loader: "postcss-loader",
             options: {
               plugins() {
-                return [autoprefixer({ browsers: "cover 99.5%" })];
+                return [autoprefixer({ overrideBrowserslist: "cover 99.5%" })];
               }
             }
           },
