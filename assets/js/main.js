355fc69993d5e901cd9b33de8eaf2f4170f87c07
ckdtjs505 <ckdtjs505@naver.com>
2019-12-30 16:40:13 +0900

    Feat: editProfile page design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 import "../scss/styles.scss";
 
-const data = async () => {
-  console.log("hello");
+const uploadTarget = document.querySelector(".upload-file");
+const uploadName = document.querySelector(".upload-name");
+
+const init = () => {
+  uploadTarget.addEventListener("change", () => {
+    let filename;
+    if (window.FileReader) {
+      // modern browser
+      filename = uploadTarget.files[0].name;
+    } else {
+      // old IE
+      filename = uploadTarget.value
+        .split("/")
+        .pop()
+        .split("\\")
+        .pop();
+    }
+    uploadName.value = filename;
+  });
 };
 
-data();
+init();
