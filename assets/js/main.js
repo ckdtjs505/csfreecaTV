a5b12e5000f8487eabf0f477c1edc532f842e317
ckdtjs505 <ckdtjs505@naver.com>
2020-01-07 21:31:26 +0900

    Fix: event listen error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,2 @@
 import "../scss/styles.scss";
-
-const uploadTarget = document.getElementById("upload-file");
-const uploadName = document.getElementById("upload-name");
-
-function init() {
-  uploadTarget.addEventListener("change", () => {
-    let filename;
-    if (window.FileReader) {
-      // modern browser
-      filename = uploadTarget.files[0].name;
-    } else {
-      // old IE
-      filename = uploadTarget.value
-        .split("/")
-        .pop()
-        .split("\\")
-        .pop();
-    }
-    uploadName.value = filename;
-  });
-}
-
-init();
+import "./upload";
---
+++ assets/js/upload.js
@@ -0,0 +1,24 @@
+const uploadTarget = document.getElementById("upload-file");
+const uploadName = document.getElementById("upload-name");
+
+function init() {
+  uploadTarget.addEventListener("change", () => {
+    let filename;
+    if (window.FileReader) {
+      // modern browser
+      filename = uploadTarget.files[0].name;
+    } else {
+      // old IE
+      filename = uploadTarget.value
+        .split("/")
+        .pop()
+        .split("\\")
+        .pop();
+    }
+    uploadName.value = filename;
+  });
+}
+
+if (uploadTarget && uploadName) {
+  init();
+}
