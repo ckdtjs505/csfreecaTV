1584a6dbe82ada3206ce7a843b45408b8d997522
ckdtjs505 <ckdtjs505@naver.com>
2020-01-08 17:19:44 +0900

    Feat: play and pause button

--- assets/js/videoPlayers.js
+++ assets/js/videoPlayers.js
@@ -5,8 +5,10 @@ const playBtn = document.getElementById("jsPlayButton");
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
 }
 
