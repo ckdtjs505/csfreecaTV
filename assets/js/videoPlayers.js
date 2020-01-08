b28feb8eab1363e8da02074ca694a2d7de1c5665
ckdtjs505 <ckdtjs505@naver.com>
2020-01-08 23:41:07 +0900

    Feat: volume button control in videoPlay

--- assets/js/videoPlayers.js
+++ assets/js/videoPlayers.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolButton");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -12,8 +13,19 @@ function handlePlayClick() {
   }
 }
 
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
