f9a974e5c9ca475c163b57ba84fd8c7009e52fd0
ckdtjs505 <ckdtjs505@naver.com>
2020-01-09 15:58:40 +0900

    Fix: end Time error

--- assets/js/videoPlayers.js
+++ assets/js/videoPlayers.js
@@ -74,7 +74,7 @@ const formatDate = seconds => {
 };
 
 function getCurrentTime() {
-  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+  currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
 }
 
 function setTotalTime() {
