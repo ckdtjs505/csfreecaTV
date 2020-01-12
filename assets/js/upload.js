47f95b915fa23f5b677fa89d34878206b44999a3
ckdtjs505 <ckdtjs505@naver.com>
2020-01-12 22:11:32 +0900

    Feat: record control in upload

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,24 +1,45 @@
+const recordContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
 const uploadTarget = document.getElementById("upload-file");
 const uploadName = document.getElementById("upload-name");
 
-function init() {
-  uploadTarget.addEventListener("change", () => {
-    let filename;
-    if (window.FileReader) {
-      // modern browser
-      filename = uploadTarget.files[0].name;
-    } else {
-      // old IE
-      filename = uploadTarget.value
-        .split("/")
-        .pop()
-        .split("\\")
-        .pop();
-    }
-    uploadName.value = filename;
-  });
+function handleUploadName() {
+  let filename;
+  if (window.FileReader) {
+    // modern browser
+    filename = uploadTarget.files[0].name;
+  } else {
+    // old IE
+    filename = uploadTarget.value
+      .split("/")
+      .pop()
+      .split("\\")
+      .pop();
+  }
+  uploadName.value = filename;
 }
 
-if (uploadTarget && uploadName) {
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+  } catch (error) {
+    recordBtn.innerHTML = "녹화 에러";
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
+
+function init() {
+  uploadTarget.addEventListener("change", handleUploadName);
+  recordBtn.addEventListener("click", startRecording);
+}
+
+if (recordContainer) {
   init();
 }
