4294a8104b33f9cba00666943f09e745d1bce3c8
ckdtjs505 <ckdtjs505@naver.com>
2020-01-23 22:06:48 +0900

    Feat: AWS S3 connect and video upload

--- localMiddleware.js
+++ localMiddleware.js
@@ -1,7 +1,21 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
 
-const multerVideo = multer({ dest: "uploads/video/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY
+});
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "csfreeca/video"
+  })
+});
+
 const multerAvatar = multer({ dest: "uploads/image/" });
 
 export const localMiddleware = (req, res, next) => {
