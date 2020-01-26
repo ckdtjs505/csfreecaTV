2094aec6056180df6c5218b01247645bb5fdfc17
ckdtjs505 <ckdtjs505@naver.com>
2020-01-26 20:17:59 +0900

    Feat: build setting finish

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import passport from "passport";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import mongoose from "mongoose";
+import path from "path";
 import { localMiddleware } from "./localMiddleware";
 import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
@@ -22,9 +23,9 @@ const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
-app.use("/img", express.static("img"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.use("/img", express.static(path.join(__dirname, "img")));
 
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/first */
+import "@babel/polyfill";
 import dotenv from "dotenv";
 
 dotenv.config();
