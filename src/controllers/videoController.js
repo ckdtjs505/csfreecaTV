54a3c69d3e6831026d3fda601c4b7bc58bfd281c
ckdtjs505 <ckdtjs505@naver.com>
2020-02-04 22:23:34 +0900

    Feat: video avatar create

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,9 @@ import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({}).sort({ _id: -1 });
+    const videos = await Video.find({})
+      .sort({ _id: -1 })
+      .populate("creator");
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
