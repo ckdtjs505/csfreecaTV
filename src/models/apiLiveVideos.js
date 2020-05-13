863d6b9a4ebeea016baef0c293242885862d23cf
ckdtjs505 <ckdtjs505@naver.com>
2020-05-13 21:57:15 +0900

    afreeca api modify

--- src/models/apiLiveVideos.js
+++ src/models/apiLiveVideos.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const clientId = process.env.AFREECATV_CLIENT_ID;
 
 export async function getAfreecaBroadList(page = 1) {
-  const url = `https://openapi.afreecatv.com/broad/list?client_id=${clientId}&select_key=cate&select_value=00130000&order_type=view_cnt&page_no=${page}`;
+  const url = `https://openapi.afreecatv.com/broad/list?client_id=${clientId}&order_type=view_cnt&page_no=${page}`;
   return axios
     .get(url)
     .then(res => {
